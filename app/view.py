f2bb13e24bf0717b9065466c22b8034650e11424
Michael Scales <mscales@mindwireless.com>
2017-12-15 13:19:21 -0600

    Add base, favicon, brand, and basic css.
    Staged basic parts for adding home view.

--- app/view.py
+++ app/view.py
@@ -2,19 +2,20 @@ from flask import render_template, g, current_app, url_for, redirect
 from app import app
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     # User module is accessed through the navigation bar
-    return redirect(
-        url_for('index')
-    )
+    # return redirect(
+    #     url_for('index')
+    # )
+    return "Hello World"
 
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('404.html', title='Page Not Found'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('500.html', title='Resource Error'), 500
