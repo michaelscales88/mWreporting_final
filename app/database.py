8d48b84ddaba1caa4d533616354b2a814eff9f76
Michael Scales <mscales@mindwireless.com>
2017-12-18 16:39:59 -0600

    Add database support.
    Added stub files for database integration with SQLAlchemy.

---
+++ app/database.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import scoped_session
+from redpanda.orm import sessionmaker
+
+from app import app
+from .model import Base
+
+# Internal db
+db_engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=db_engine))
+
+# Source db
+pg_engine = create_engine(app.config['PG_CONNECTION'])
+pg_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=pg_engine))
+
+# MS SQL db
+# ms_engine = create_engine(app.config['MSSQL_CONNECTION'])
+# ms_session = scoped_session(sessionmaker(autocommit=False,
+#                                          autoflush=False,
+#                                          bind=ms_engine))
+
+# Bind session to the ORM model query method
+Base.query = db_session.query_property()
+
+
+def init_db():
+
+    # Import models to include in the Base
+
+    from app.user import User
+    from app.report import EventTable, CallTable, ReportCache
+
+    Base.metadata.create_all(bind=db_engine)
+
+
+@event.listens_for(pg_engine, 'begin')
+def receive_begin(conn):
+    conn.execute('SET TRANSACTION READ ONLY')
+
+# Create metadata during app
+# initialization after the engine is created
+init_db()
---
+++ database/config_db.py
@@ -0,0 +1,37 @@
+from collections import OrderedDict
+
+
+def db_menu():
+    manager = DbManager()
+    manager.config_from_object(default_config.Config)
+    manager.base = Base
+    commands = OrderedDict(
+        [
+            ('Create DB', manager.create),
+            ('Upgrade DB', manager.upgrade),
+            ('Downgrade DB', manager.downgrade),
+            ('Migrate DB', manager.migrate),
+            ('Quit', '')
+        ]
+    )
+    idx_choices = list(enumerate(commands.keys()))
+    while True:
+        idx_choice = int(input(idx_choices))
+        cmd = idx_choices[idx_choice][1]
+        exc_cmd = commands[cmd]
+        if exc_cmd:
+            exc_cmd()
+        else:
+            break
+
+
+if __name__ == '__main__':
+    import sys
+    from os import path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+    from app import default_config
+    from app.database import Base
+    from .db_manager import DbManager
+
+    db_menu()
