a81b66223f198571741d0d93ec02dbf5342492d2
Michael Scales <mscales@mindwireless.com>
2018-01-14 12:27:23 -0600

    add export support

--- app/util/update_cdn.py
+++ app/util/update_cdn.py
@@ -2,11 +2,20 @@ from flask_bootstrap import WebCDN
 
 
 def add_cdns(app):
+    # Date and Time selector
     app.extensions['bootstrap']['cdns']['bootstrapdtp'] = WebCDN(
         'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/'
     )
-    app.extensions['bootstrap']['cdns']['dataTables'] = WebCDN(
+    # DataTable for displaying information in a grid
+    app.extensions['bootstrap']['cdns']['dataTable'] = WebCDN(
         'https://cdn.datatables.net/1.10.16/'
+    )
+    # Buttons for saving/printing
+    app.extensions['bootstrap']['cdns']['dataTableBtns'] = WebCDN(
+        'https://cdn.datatables.net/buttons/1.5.1/'
+    )
+    app.extensions['bootstrap']['cdns']['saveJs'] = WebCDN(
+        'https://cdnjs.cloudflare.com/ajax/libs/'
     )
 
 
