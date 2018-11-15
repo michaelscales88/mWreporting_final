53e15a6c6e13c51e001d249ecdfe292204341fed
Michael Scales <mhs42@txstate.edu>
2018-11-14 18:04:29 -0600

    working on celery

--- modules/celery_worker.py
+++ modules/celery_worker.py
@@ -2,20 +2,15 @@ from modules import app
 from celery import Celery
 
 
-# def create_celery(app_instance):
-#
-#
-#     class ContextTask(celery.Task):
-#         abstract = True
-#
-#         def __call__(self, *args, **kwargs):
-#             with app_instance.app_context():
-#                 return super().__call__(self, *args, **kwargs)
-#
-#     celery.Task = ContextTask
-#     return celery
-
-
 with app.app_context():
     celery = Celery(__name__, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return super().__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
