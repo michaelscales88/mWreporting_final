ee098c7d2059bb4ddffa22627a0ba614e7878362
Michael Scales <mhs42@txstate.edu>
2018-11-16 15:08:46 -0600

    add seeding for reports

---
+++ seed_load_dates.py
@@ -0,0 +1,23 @@
+import datetime
+from modules import app
+from modules.core import get_model_by_tablename
+
+
+def add_dates():
+
+    with app.app_context():
+        start_dt = datetime.datetime.strptime(str(input("What date to start?")), "%d/%m/%Y")
+        days_to_load = int(input("How many days to load from the start time?"))
+        counter = 0
+
+        load_table = get_model_by_tablename("loaded_tables")
+
+        while counter < days_to_load:
+            load_table.create(loaded_date=start_dt + datetime.timedelta(days=counter))
+            counter += 1
+
+        load_table.session.commit()
+
+
+if __name__ == '__main__':
+    add_dates()
---
+++ seed_sla_reports.py
@@ -0,0 +1,27 @@
+import datetime
+from modules import app
+from modules.core import get_model_by_tablename
+
+
+def add_reports():
+
+    with app.app_context():
+        start_dt = datetime.datetime.strptime(str(input("What date to start?")), "%d/%m/%Y")
+        days_to_load = int(input("How many days to load from the start time?"))
+        counter = 0
+
+        sla_report_model = get_model_by_tablename("sla_report")
+
+        while counter < days_to_load:
+            date = start_dt + datetime.timedelta(days=counter)
+            sla_report_model.create(
+                start_time=date.replace(hour=7, minute=0, second=0, microsecond=0),
+                end_time=date.replace(hour=19, minute=0, second=0, microsecond=0)
+            )
+            counter += 1
+
+        sla_report_model.session.commit()
+
+
+if __name__ == '__main__':
+    add_reports()
