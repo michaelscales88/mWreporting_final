4b970ef9a70db19c2f0ff0b44a62f1fdb4d79c7d
Michael Scales <mhs42@txstate.edu>
2018-12-20 12:39:42 -0700

    fix celery

--- modules/celery_worker.py
+++ modules/celery_worker.py
@@ -1,9 +1,11 @@
 from modules import app
 from celery import Celery
+from json import dumps
 
 
 with app.app_context():
     celery = Celery(__name__, broker=app.config['CELERY_BROKER_URL'])
+    print(dumps(app.config, indent=2, default=str))
     celery.conf.update(app.config)
 
     class ContextTask(celery.Task):
